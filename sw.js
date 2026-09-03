51ca4fa7fab9775af8c860025340f4e26c8f3153
rigantijuanpablo-prog <rigantijuanpablo@gmail.com>
2026-09-02 23:38:31 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open('rgma-v4').then(c=>c.addAll([
+      './app.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png',
+      './logo-dorado-circulo-blanco.jpg'
+    ]))
+  );
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
